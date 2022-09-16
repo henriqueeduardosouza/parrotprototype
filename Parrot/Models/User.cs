7872ed49a8e1b4d8da8a8f6b92739b3961bdb394
cteds_11 <cteds_11@LABPROG11>
2022-09-16 20:30:06 -0300

    Add project files.

---
+++ Parrot/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parrot.Models {
+    public abstract class User {
+        string Name { get; set; }
+        string Id { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+        List<string> Friends { get; set; }
+    }
+}
